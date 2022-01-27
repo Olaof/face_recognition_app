74b1162202b21328760fa4316c2de59adf111f72
Jean-Marc <jeanmarc.desiront@gmail.com>
2022-01-27 18:46:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,181 @@
-import logo from './logo.svg';
+
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Particles from 'react-tsparticles';
+import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import './App.css';
 
-function App() {
-  return (
+window.process = {
+   env: {
+     NODE_ENV: 'development'
+   }
+}
+
+
+  const particlesInit = (main) => {
+    console.log(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const particlesOptions = {
+        background: {
+          color: {
+            value: "#d1913c1",
+          },
+        },
+        fpsLimit: 40,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 3,
+              opacity: 0.6,
+              size: 20,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#e6dc0c",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.6,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 7,
+          },
+        },
+        detectRetina: true,
+      }
+
+  const app = new Clarifai.App({
+    apiKey: 'dbf2710547434915b8d357b9969df30a'
+  });
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+      const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+      const image = document.getElementById('inputimage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height),
+      }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+      this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    //setImageUrl(input);
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict( 
+      Clarifai.FACE_DETECT_MODEL,
+      //'53e1df302c079b3db8a0a36033ed2d15', 
+      this.state.input )
+    .then((response) => {
+      //console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      this.displayFaceBox(this.calculateFaceLocation(response));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+
+  render() {
+    //const { imageUrl } = this.state;
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Particles className="particles"
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={particlesOptions}
+       />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
+
+
